b4b37fd637e2832e2393152e7ad8c42dad4d965e
Satya Sundar Sahu <tinkusahu.com@gmail.com>
2019-09-01 23:59:14 +0530

    Added New program like bright ness correction rotation
    Signed-off-by: Satya Sundar Sahu <tinkusahu.com@gmail.com>

--- program/my_header.h
+++ program/my_header.h
@@ -8,11 +8,11 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define IMG_NAME_BUFF_LENGTH	16
-#define IMG_NAME_PATH_LENGTH	256
+#define IMG_NAME_BUFF_LENGTH	32
+#define IMG_NAME_PATH_LENGTH	512
 //#define IMG_SEARCH_PATH		"~/satya_git/meta-image-processing/TestImages"
-//#define IMG_SEARCH_PATH		"/home/satya/satya_git/meta-image-processing/TestImages"
-#define IMG_SEARCH_PATH		"/home/nullbyte/satya_bitbucket/my-git/meta-image-processing/TestImages"
+#define IMG_SEARCH_PATH		"/home/satya/satya_git/meta-image-processing/TestImages"
+//#define IMG_SEARCH_PATH		"/home/nullbyte/satya_bitbucket/my-git/meta-image-processing/TestImages"
 
 #define BMP_HEADER_SIZE         54
 #define BMP_COLOR_TABLE_SIZE    1024
@@ -41,8 +41,28 @@ struct image_writer {
 	int bitDepth;
 };
 
+struct image_reader_mat {
+	char image_name[IMG_NAME_PATH_LENGTH];
+	int height;
+	int width;
+	int bitDepth;
+	unsigned char header[BMP_HEADER_SIZE];
+	unsigned char colorTable[BMP_COLOR_TABLE_SIZE];
+	unsigned char **buf;
+};
+
+struct image_writer_mat {
+	char new_image_name[IMG_NAME_PATH_LENGTH];
+	unsigned char header[BMP_HEADER_SIZE];
+	unsigned char colorTable[BMP_COLOR_TABLE_SIZE];
+	int bitDepth;
+	unsigned char **buf;
+};
+
 int bmp_image_read(struct image_reader *img_rd);
 int bmp_image_write(struct image_writer *img_wr);
+int bmp_image_read_mat(struct image_reader_mat *img_rd);
+int bmp_image_write_mat(struct image_writer_mat *img_wr, int);
 int image_copy();
 int bmp_rgb_to_gry();
 int bmp_img_to_blkwhi();
@@ -50,3 +70,5 @@ int bmp_img_increase_bright();
 int bmp_img_reduce_grey_level();
 int bmp_creat_histogram();
 int bmp_histogram_equ();
+int bmp_brightness_correction();
+int bmp_image_rotation();
